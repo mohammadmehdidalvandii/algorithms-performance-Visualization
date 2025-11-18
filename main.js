3b741382f1a397a92de894ced56d81e8b910c93b
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-11-18 15:49:16 +0330

    fixed bubble sort

--- main.js
+++ main.js
@@ -19,6 +19,57 @@ function renderArray() {
     });
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+async function bubbleSort() {
+    const bars = document.querySelectorAll(".bar");
+
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = 0; j < array.length - i - 1; j++) {
+
+            bars[j].style.background = "yellow";
+            bars[j + 1].style.background = "yellow";
+
+            await sleep(80);
+
+            if (array[j] > array[j + 1]) {
+                bars[j].style.background = "red";
+                bars[j + 1].style.background = "red";
+
+                await sleep(80);
+
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                bars[j].style.height = array[j] + "px";
+                bars[j + 1].style.height = array[j + 1] + "px";
+
+                await sleep(80);
+            }
+
+            bars[j].style.background = "#4a90e2";
+            bars[j + 1].style.background = "#4a90e2";
+        }
+
+        bars[array.length - 1 - i].style.background = "green";
+    }
+
+    bars[0].style.background = "green";
+}
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    const algo = document.getElementById("algoritmSelect").value;
+
+    if (algo === "bubble") {
+        bubbleSort();
+    }
+}); 
+
+
 document.getElementById("generateBtn").addEventListener("click", () => {
     generateArray();
 });
